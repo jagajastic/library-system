667e795b57f66a6434008d6ba0e383b7d1c934c0
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-16 14:52:03 +0100

    Add admin constructor

---
+++ src/constructors/admin.js
@@ -0,0 +1,21 @@
+// import user constructor
+var User = require("./user");
+// import Request constructor
+var Request = require("./request");
+// import assign id function
+var assignId = require("../../helpers/assign-id");
+
+// Admin constructor
+function Admin(username, password) {
+  this.id = assignId("admin");
+  this.username = username;
+  this.password = password;
+}
+
+// Inherit request method
+Admin.prototype = Object.create(Request.prototype);
+// re-assign Admin connstructor
+Admin.prototype.constructor = Admin;
+
+// export Admin constructor
+module.exports = Admin;
