b65ba939f8e4c1e646f920469543c45b1c217a29
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-17 06:58:58 +0100

    Add update method to Book prototype

--- src/constructors/book.js
+++ src/constructors/book.js
@@ -1,9 +1,9 @@
 // import assign id function
-var assignId = require('../../helpers/assign-id');
+var assignId = require("../../helpers/assign-id");
 // import book
-var findBook= require("../../helpers/find-book");
-// import db 
-var db = require('../../database');
+var findBook = require("../../helpers/find-book");
+// import db
+var db = require("../../database");
 
 // books
 function Book(title, category, author, quantity) {
@@ -16,17 +16,32 @@ function Book(title, category, author, quantity) {
 
 // create book mehtod
 Book.prototype.create = function() {
-// add new book record to database
+  // add new book record to database
   return db.books.push(this);
 };
 
-// read book 
-Book.prototype.read = function (id) {
-    return findBook(id);
-}
+// read book
+Book.prototype.read = function(id) {
+  return findBook(id);
+};
+
+// update method
+Book.prototype.update = function(data, id) {
+  var book = findBook(id);
+  if (book) {
+    book.title = data.title;
+    book.category = data.category;
+    book.author = data.author;
+    book.quantity = data.quantity;
+    return true;
+  } else {return false;}
+  console.log(db.books);
+  
+};
 // var book1 = new Book('new school', 'maths', 'Gandi Bi', 3);
 // book1.create();
 // console.log(book1.read(90));
+// console.log(book1.update({title: 'unew', category: 'umath', author: 'uauhtor', quantity: 5}, 12));
 // console.log(db.books);
 // export book constructor
 module.exports = Book;
