d4d39599c556a90d784eeb9f6942bf1edffe4c84
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-17 03:54:54 +0100

    Add crate, read method to User constructor

--- src/constructors/book.js
+++ src/constructors/book.js
@@ -1,13 +1,32 @@
-// import assign id function 
+// import assign id function
 var assignId = require('../../helpers/assign-id');
+// import book
+var findBook= require("../../helpers/find-book");
+// import db 
+var db = require('../../database');
+
 // books
-function Book (title, category, author, quantity){
-    this.id = assignId('books');
-    this.title = title;
-    this.category = category;
-    this.author = author;
-    this.quantity = quantity;
+function Book(title, category, author, quantity) {
+  this.id = assignId("books");
+  this.title = title;
+  this.category = category;
+  this.author = author;
+  this.quantity = quantity;
+}
+
+// create book mehtod
+Book.prototype.create = function() {
+// add new book record to database
+  return db.books.push(this);
 };
 
+// read book 
+Book.prototype.read = function (id) {
+    return findBook(id);
+}
+// var book1 = new Book('new school', 'maths', 'Gandi Bi', 3);
+// book1.create();
+// console.log(book1.read(90));
+// console.log(db.books);
 // export book constructor
 module.exports = Book;
