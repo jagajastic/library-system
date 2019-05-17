1f7eb4f55e5ac9c45a6aef39135b7c1ef215030a
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-17 16:17:04 +0100

    Add read request method test

--- tests/request.test.js
+++ tests/request.test.js
@@ -17,7 +17,7 @@ describe("Create method Test", function() {
     var book1 = new Book("new school", "physics", "Gandi bi", 1);
     book1.create();
     // creat another book
-    var book2 = new Book('new school chemistry', 'chemistry', 0)
+    var book2 = new Book("new school chemistry", "chemistry", 0);
     // create user
     var student = new User("joe", "pass", 3);
     student.create();
@@ -41,5 +41,58 @@ describe("Create method Test", function() {
   test("if book is taken", function() {
     var req1 = new Request(2, 1);
     expect(req1.create()).toBeFalsy();
+  });
+});
+
+// read request method
+describe("Read request Method test", function() {
+  beforeAll(function() {
+    db.users.length = 0;
+    db.books.length = 0;
+    db.request.length = 0;
+    // create book
+    var book1 = new Book("new school", "physics", "Gandi bi", 4);
+    book1.create();
+    // creat another book
+    var book2 = new Book("new school chemistry", "chemistry", 0);
+    book2.create();
+    // create user
+    var student = new User("joe", "pass", 3);
+    student.create();
+  });
+
+  test("if request is found", function() {
+    var req1 = new Request(1, 1);
+    req1.create();
+    expect(req1.read(1)).toMatchObject({
+      bookId: 1,
+      id: 1,
+      status: 0,
+      userId: 1
+    });
+  });
+
+  test("if request id is nnot passed", function() {
+    var req1 = new Request(1, 1);
+    req1.create();
+    expect(req1.read()).toBeFalsy();
+  });
+
+  test("if request id is not found", function() {
+    var req1 = new Request(1, 1);
+    req1.create();
+    expect(req1.read(5)).toBeFalsy();
+  });
+
+  test("if request id is zero", function() {
+    var req1 = new Request(1, 1);
+    req1.create();
+    expect(req1.read(0)).toBeFalsy();
+  });
+
+  test("if request id is undefined", function() {
+    var req1 = new Request(1, 1);
+    req1.create();
+    expect(req1.read(undefined)).toBeFalsy();
   });
 });
