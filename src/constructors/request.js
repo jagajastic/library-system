11e228c94903add24fa086dab06a6254e923225b
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-17 15:25:09 +0100

    Add create request method to request prototype

--- src/constructors/request.js
+++ src/constructors/request.js
@@ -1,5 +1,59 @@
+// import find-user helper function
+var findUser = require("../../helpers/find-user");
+// import assign-id function
+var assignId = require("../../helpers/assign-id");
+// import assign-id helper function
+var findBook = require("../../helpers/find-book");
+// import book
+var Book = require("../constructors/book");
+// import users
+var User = require("../constructors/user");
+// import database
+var db = require("../../database");
+
 // request constructor
-function Request() {}
+function Request(bookId, userId) {
+  this.id = assignId("request");
+  this.bookId = bookId;
+  this.userId = userId;
+  this.status = 0;
+}
+
+// create request method
+Request.prototype.create = function() {
+  // check if user, book exist
+  if (!findUser(this.userId) || !findBook(this.bookId)) {
+    // return false for book/user do not exist
+    return false;
+  }
+  //   check if book is available
+  if (findBook(this.bookId).quantity === 0) {
+    // if not available, return book taken
+    return "book taken";
+  }
+
+  return db.request.push(this);
+};
+
+// read method
+Request.prototype.read = function (id) {
+  // return false if id is undefind
+  // if (!id){
+  //   return false;
+  // }
+
+  // return db.
+}
+// create book
+// var book1 = new Book("new school", "physics", "Gandi bi", 1);
+// book1.create();
+// // create user
+// var student = new User("joe", "pass", 3);
+// student.create();
+// // request obj
+// var req1 = new Request(9, 1);
+// console.log(req1.create());
+// console.log(db);
 
 // export Request constructor
 module.exports = Request;
