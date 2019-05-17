c7a10ad5f9007dc3681d1c93472d124f123044a6
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-17 16:51:49 +0100

    Add update method to request prototype

--- src/constructors/request.js
+++ src/constructors/request.js
@@ -11,7 +11,7 @@ var User = require("../constructors/user");
 // import database
 var db = require("../../database");
 // import find-request
-var findRequest = require('../../helpers/find-request');
+var findRequest = require("../../helpers/find-request");
 
 // request constructor
 function Request(bookId, userId) {
@@ -38,25 +38,47 @@ Request.prototype.create = function() {
 };
 
 // read method
-Request.prototype.read = function (id) {
+Request.prototype.read = function(id) {
   // return false if id is undefind
-  if (!id){
+  if (!id) {
     return false;
   }
-// return the request when found, else return false
+  // return the request when found, else return false
   return findRequest(id);
-}
+};
+
+// update method
+Request.prototype.update = function(data, id) {
+  // check if id is undefined
+  if (!id) {
+    return false;
+  }
+  var request = findRequest(id);
+  if (request) {
+    // change the value of the bookId,
+    request.bookId = data.bookId;
+    // return true after updating
+    return true;
+  }
+  // return false if request not found
+  return request;
+};
 // create book
 // var book1 = new Book("new school", "physics", "Gandi bi", 1);
 // book1.create();
+// // create another book
+// var book2 = new Book("new school chemistry", "chemistry", "Gandi bi", 1);
+// book2.create();
 // // create user
 // var student = new User("joe", "pass", 3);
 // student.create();
 // // request obj
 // var req1 = new Request(1, 1);
 // console.log(req1.create());
+// req1.update({bookId: 2}, 1);
 // console.log(req1.read(1));
-// console.log(db);
+
+console.log(db.request);
 
 // export Request constructor
 module.exports = Request;
