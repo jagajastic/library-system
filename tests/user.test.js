919064921de9d09451f078418f2475f9fdb52418
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-19 00:15:53 +0100

    import needed constructor

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,7 +3,9 @@ var User = require("../src/constructors/user");
 // import database
 var db = require("../database");
 // import book
-var Book = require('../src/constructors/book');
+var Book = require("../src/constructors/book");
+// import user constructor
+var User =  require("../src/constructors/user");
 
 // User constructor suite
 describe("User Constructor Test ", function() {
@@ -73,86 +75,90 @@ describe("Update Test", function() {
   });
 });
 
-
 // delete method
-describe('Delete method Test', function () {
-  test('if record is deleted from database', function () {
+describe("Delete method Test", function() {
+  test("if record is deleted from database", function() {
     var user1 = new User("lot", "pass", 1);
     expect(user1.delete(1)).toHaveLength(1);
   });
 
-  test('if record is not found', function () {
+  test("if record is not found", function() {
     var user1 = new User("lot", "pass", 1);
     expect(user1.delete(90)).toBeFalsy();
   });
-  
-  test('if record id is undefined', function () {
+
+  test("if record id is undefined", function() {
     var user1 = new User("lot", "pass", 1);
     expect(user1.delete()).toBeFalsy();
   });
 });
 
 //book request method
-describe('Book Request Test', function () {
-  beforeAll( function () {
+describe("Book Request Test", function() {
+  beforeAll(function() {
     db.users.length = 0;
     db.books.length = 0;
     db.request.length = 0;
-    var book1 = new Book('javascript', 'programming', 'ninja', 4);
+    var book1 = new Book("javascript", "programming", "ninja", 4);
     book1.create();
-    var book2 = new Book('css', 'programming', 'ninja', 4);
+    var book2 = new Book("css", "programming", "ninja", 4);
     book2.create();
-    var book3 = new Book('html', 'programming', 'ninja', 4);
+    var book3 = new Book("html", "programming", "ninja", 4);
     book3.create();
-    var book4 = new Book('css', 'programming', 'ninja', 4);
+    var book4 = new Book("css", "programming", "ninja", 4);
     book4.create();
   });
 
-  test('if user can create book request', function () {
-    var user1 = new User('jam', 'pass', 0);
+  test("if user can create book request", function() {
+    var user1 = new User("jam", "pass", 0);
     user1.create();
-    expect(user1.createBookRequest({bookId: 3, userId: 1})).toBe(1);
+    expect(user1.createBookRequest({bookId: 1, userId: 1, userPriority: 3})).toBe(1);
   });
 
   test('if user id is wrong', function () {
     var user1 = new User('cassie', 'try', 0);
     user1.create();
-    expect(user1.createBookRequest({bookId: 1, userId: 0})).toBeFalsy();
+    expect(user1.createBookRequest({bookId: 1, userId: 0, userPriority: 3})).toBeFalsy();
   });
 
   test('if book id is wrong', function () {
     var user1 = new User('jumal', 'tete', 0);
     user1.create();
-    expect(user1.createBookRequest({bookId: 0, userId: 1})).toBeFalsy();
+    expect(user1.createBookRequest({bookId: 0, userId: 1, userPriority: 3})).toBeFalsy();
   });
 });
 
-// read request method 
-describe('Read book Request Test', function () {
-  beforeAll( function () {
+// read request method
+describe("Read book Request Test", function() {
+  beforeAll(function() {
     db.users.length = 0;
     db.books.length = 0;
     db.request.length = 0;
-    var book1 = new Book('javascript', 'programming', 'ninja', 4);
+    var book1 = new Book("javascript", "programming", "ninja", 4);
     book1.create();
-    var book2 = new Book('css', 'programming', 'ninja', 4);
+    var book2 = new Book("css", "programming", "ninja", 4);
     book2.create();
-    var book3 = new Book('html', 'programming', 'ninja', 4);
+    var book3 = new Book("html", "programming", "ninja", 4);
     book3.create();
-    var book4 = new Book('css', 'programming', 'ninja', 4);
+    var book4 = new Book("css", "programming", "ninja", 4);
     book4.create();
-    var user1 = new User('jumal', 'tete', 0);
+    var user1 = new User("jumal", "tete", 0);
     user1.create();
-    user1.createBookRequest({bookId: 2, userId: 1});
-    var user2 = new User('jumal', 'tete', 0);
+    user1.createBookRequest({ bookId: 2, userId: 1, userPriority: 3 });
+    var user2 = new User("jumal", "tete", 0);
     user2.create();
-    user2.createBookRequest({bookId: 1, userId: 1});
+    user2.createBookRequest({ bookId: 1, userId: 1, userPriority: 3 });
   });
 
-  test('if read book request return the book', function () {
-    var user1 = new User('Kasim', 'otak', 0);
+  test("if read book request return the book", function() {
+    var user1 = new User("Kasim", "otak", 0);
     user1.create();
-    expect(user1.readBookRequest(1)).toMatchObject({"bookId": 2, "id": 1, "status": 0, "userId": 1});
+    expect(user1.readBookRequest(1)).toMatchObject({
+      bookId: 2,
+      id: 1,
+      status: 'pending',
+      userId: 1
+    });
   });
 
   test('if read book request id does not exist', function () {
@@ -165,5 +171,11 @@ describe('Read book Request Test', function () {
     var user1 = new User('Kasim', 'otak', 0);
     user1.create();
     expect(user1.readBookRequest()).toBeFalsy();
+  });
+
+  test('if read book request id is null', function () {
+    var user1 = new User('Kasim', 'otak', 0);
+    user1.create();
+    expect(user1.readBookRequest(null)).toBeFalsy();
   });
 });
