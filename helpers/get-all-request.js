06c05df4ea4e6e0fcd1829f42bea32475c984ad3
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-19 00:09:02 +0100

    Add get-alll-request function to helper

---
+++ helpers/get-all-request.js
@@ -0,0 +1,20 @@
+// import db
+var db = require("../database");
+
+// get all request from db based on priority
+function getAllRequest() {
+  // sorted array by priority
+  var sortedBookRequest = db.request.sort(function(a, b) {
+    return a.userPriority - b.userPriority;
+  });
+  // loop through the array for same book request
+  for (index = 0; index < sortedBookRequest.length; index++) {
+    sortedBookRequest[index].status = "Approved";
+  }
+  //give the one with higher priority
+  //give other based on priority
+  return true;
+}
+
+// import getAllRequest function
+module.exports = getAllRequest;
