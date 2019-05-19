d36ab43b80e9994b82c2ab7222ae9e0a4784dcbb
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-19 06:52:34 +0100

    Add loop, loop over book request and approve all books

--- helpers/get-all-request.js
+++ helpers/get-all-request.js
@@ -4,17 +4,10 @@ var db = require("../database");
 // get all request sorted by priority from db based
 function getAllRequest() {
   // sorted array by priority
-  var sortedBookRequest = db.request.sort(function(a, b) {
+  return  db.request.sort(function(a, b) {
     return a.userPriority - b.userPriority;
   });
-  // loop through the array for same book request
-  for (index = 0; index < sortedBookRequest.length; index++) {
-    sortedBookRequest[index].status = "Approved";
-  }
-  //give the one with higher priority
-  //give other based on priority
-  return true;
 }
 
-// import getAllRequest function
+// export getAllRequest function
 module.exports = getAllRequest;
