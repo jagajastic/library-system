f2b48847d3a1932100c35075e893d375691cbf7b
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-19 06:54:03 +0100

    Remove unused import, add delete, delete all  function

--- src/constructors/admin.js
+++ src/constructors/admin.js
@@ -1,11 +1,13 @@
 // import user constructor
 var User = require("./user");
-// import Request constructor
-var Request = require("./request");
 // import assign id function
 var assignId = require("../../helpers/assign-id");
 // import getAllRequest helper function
 var getAllRequest = require("../../helpers/get-all-request");
+// import find request
+var findRequest = require('../../helpers/find-request');
+// import database
+var db = require('../../database');
 
 // Admin constructor
 function Admin(username, password, priority) {
@@ -24,11 +26,42 @@ Admin.prototype.approveBookRequest = function() {
   if (this.priority !== 4) {
     return false;
   }
-  // get all book request sorted by priority
-  return getAllRequest();
+  // get all request sorted by priority
+  var sortedBookRequest = getAllRequest();
+  // loop through the array for same book request
+  for (index = 0; index < sortedBookRequest.length; index++) {
+    sortedBookRequest[index].status = "Approved";
+  }
+  // return true after approval
+  return true;
 };
 
-// var admin = new Admin("admin", "amdin", 0);
-// console.log(getAllRequest());
+// delete single request method
+Admin.prototype.delete = function(id) {
+  // check if id is not defined
+  if (!id) {
+    return false;
+  }
+  // console.log(findRequest(id));
+  
+  if (!findRequest(id)) {
+    return false;
+  }
+  // get all request
+  var allRequest = getAllRequest();
+  // loop over all request to find by id
+  for (var index = 0; index < allRequest.length; index++) {
+    if (allRequest[index].id === id) {
+      return allRequest.splice(index, 1);
+    }
+  }
+};
+
+// delete all request by admin
+Admin.prototype.deleteAll = function () {
+var deleteALlRequest = db.request.length = 0
+  return deleteALlRequest;
+}
+
 // export Admin constructor
 module.exports = Admin;
