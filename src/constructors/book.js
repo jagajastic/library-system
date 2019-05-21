f56f1d8671aa36d54203df1339ec2fae55cc3b8a
Ibrahim Joseph <josephibrahi@gmail.com>
2019-05-21 23:01:25 +0100

    Add global flag to regex

--- src/constructors/book.js
+++ src/constructors/book.js
@@ -1,13 +1,13 @@
 // import assign id function
-var assignId = require('../../helpers/assignId');
+var assignId = require("../../helpers/assignId");
 // import book
-var findBook = require('../../helpers/findBook');
+var findBook = require("../../helpers/findBook");
 // import db
-var db = require('../../database');
+var db = require("../../database");
 
 // books
 function Book(title, category, author, quantity) {
-  this.id = assignId('books');
+  this.id = assignId("books");
   this.title = title;
   this.category = category;
   this.author = author;
@@ -57,11 +57,11 @@ Book.prototype.deleteBook = function(id) {
 // search book method by title
 Book.prototype.searchBook = function(bookTitle) {
   // return false if book title is undefined
-  if (!bookTitle || typeof bookTitle === 'number') {
+  if (!bookTitle || typeof bookTitle === "number") {
     return false;
   }
   // regex pattern to check with book title
-  var pattern = new RegExp(bookTitle, 'g');
+  var pattern = new RegExp(bookTitle, "g");
   // empty array to store match found
   var foundTitle = [];
   // loop through book array, return all booktitle match
